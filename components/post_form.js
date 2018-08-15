d236b745d22939e2c89e4a11fc419ae954168574
Matthew Kruk <matthew.kruk@gmail.com>
2018-08-15 09:52:10 -0400

    add directive, show/hide

--- components/post_form.js
+++ components/post_form.js
@@ -7,9 +7,9 @@ const postForm = {
     template: `
     <form class="form__new-post" ng-submit="$ctrl.onSubmit({ newPost: $ctrl.postContent });">
         <label for="title">Title</label>
-        <input class="input__title" type="text" ng-model="$ctrl.postContent.title" placeholder="Post title">
+        <input expand-on-focus class="input__title" type="text" ng-model="$ctrl.postContent.title" placeholder="Post title">
         <label for="text">Thought</label>
-        <textarea class="textarea__text" ng-model="$ctrl.postContent.text"></textarea>
+        <textarea expand-on-focus class="textarea__text" ng-model="$ctrl.postContent.text"></textarea>
         <button class="btn__submit">Add Post</button>
     </form>
     `
--- components/social_posts.js
+++ components/social_posts.js
@@ -4,9 +4,9 @@ const socialPosts = {
     template: `
         <h1 class="h1">My Thoughts</h1>
         <section class="section__container">
-            <button class="btn__add-post" type="button">New Thought</button>
+            <button class="btn__add-post" type="button" ng-click="$ctrl.toggleDisplay();">New Thought</button>
             <section class="section__form">
-                <post-form on-submit="$ctrl.onSubmit(newPost);"></post-form>
+                <post-form ng-hide="$ctrl.toggleElements" on-submit="$ctrl.onSubmit(newPost);"></post-form>
             </section>
             <section class="section__posts">
                 <post ng-repeat="post in $ctrl.postList" post="post"></post>
@@ -26,13 +26,41 @@ const socialPosts = {
                 text: 'This is my first post, great to be here!'
             }
         ]
+        vm.toggleElements = true;
+
+        vm.toggleDisplay = () => {
+            vm.toggleElements = false;
+        };
         vm.onSubmit = (newPost) => {
-            console.log("adding new post:" + newPost.title);
+            vm.toggleElements = true;
+            console.log("adding new post: " + newPost.title);
+            //could also pass angular.copy(newPost)
             vm.postList.unshift({
                 title: newPost.title,
                 text: newPost.text
             });
         };
+
+        function expandOnFocus() {
+            return {
+                restrict: "A",
+                replace: false,
+                link: function ($scope, $element, $attrs) {
+                    $element.on("focus", () => {
+                        $element
+                    .css("width", "200px")
+                    .css("height", "50px")
+                    .css("font-size", "40px")
+                    });
+                $element.on("blur",  () => {
+                    $element
+                    .css("width", "")
+                    .css("height", "")
+                    .css("font-size", "")
+                });
+                }
+            };
+        }
     }
 
 
